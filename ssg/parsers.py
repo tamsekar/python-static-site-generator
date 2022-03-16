b59f86379b7ee7fae449d0ab8cfe3bf68647924e
atamsekar <atamsekar@pulsesecure.net>
2022-03-16 01:03:20 -0700

    Module2 parser class

---
+++ ssg/parsers.py
@@ -0,0 +1,9 @@
+from typing import List
+from pathlib import Path
+
+class Parser:
+    extentions: List[str] = []
+    
+    def valid_extension(extension):
+        return self.extensions(extension)
+    
