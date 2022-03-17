060b9c097678617c2d0df033346c24741fcd67c4
atamsekar <atamsekar@pulsesecure.net>
2022-03-16 22:04:00 -0700

    Added Subclass and parse methods

--- ssg/site.py
+++ ssg/site.py
@@ -2,10 +2,11 @@ from pathlib import Path
 
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
-    
+        self.parsers = parsers or []
+        
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
@@ -16,4 +17,18 @@ class Site:
             
             if path.is_dir():
                 self.create_dir(path)
-    
+            elif path.is_file():
+                self.run_parser(path)
+                
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+            
+    def run_parser(self, path):
+        parser = load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, source, dest)
+        else:
+            print('Not Implemented')
+            
